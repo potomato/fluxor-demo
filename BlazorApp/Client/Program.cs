d743a3ea2e2c36d195cc2f67d0c2b1554aaea3b6
tom.abnett <tom.abnett@correla.com>
2022-05-18 11:57:26 +0100

    Add Fluxor.Blazor.Web

--- BlazorApp/Client/Program.cs
+++ BlazorApp/Client/Program.cs
@@ -1,11 +1,15 @@
 using BlazorApp.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Fluxor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var currentAssembly = typeof(Program).Assembly;
+builder.Services.AddFluxor(options => options.ScanAssemblies(currentAssembly));
 
 await builder.Build().RunAsync();
